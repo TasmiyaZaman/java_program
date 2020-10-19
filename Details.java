8ef1716bfc550b5f8fd59c514e59459f52d0a28d
TasmiyaZaman <72901321+TasmiyaZaman@users.noreply.github.com>
2020-10-19 10:14:20 -0700

    Add files via upload
    Here is a java program to store the student details.

---
+++ Details.java
@@ -0,0 +1,10 @@
+class Details{
+String studentname;
+String rollno;
+String branch;
+void AIT()
+{
+
+System.out.println(studentname+" " + rollno+ " " + branch+" "+ "AIT"  );
+}
+}
---
+++ Detailsofstudent.java
@@ -0,0 +1,28 @@
+class Detailsofstudent{
+public static void main(String args[])
+{
+Details stud=new Details();
+stud.studentname="Tasmiya";
+stud.rollno="4AI16EC007";
+stud.branch="EC";
+stud.AIT();
+
+
+stud.studentname="Usha";
+stud.rollno="4AI16EC015";
+stud.branch="EC";
+stud.AIT();
+
+stud.studentname="Varshitha";
+stud.rollno="4ai16IS015";
+stud.branch="IS";
+stud.AIT();
+
+stud.studentname="Ramya";
+stud.rollno="4AI16CS015";
+stud.branch="CS";
+stud.AIT();
+
+
+} 
+}
