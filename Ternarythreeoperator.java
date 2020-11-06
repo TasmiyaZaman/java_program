8367e752b22d1b59e37cfad4c755867720cdf384
TasmiyaZaman <72901321+TasmiyaZaman@users.noreply.github.com>
2020-11-06 22:34:29 +0530

    Add files via upload
    Ternary operator example program.

---
+++ Ternarythreeoperator.java
@@ -0,0 +1,14 @@
+package com.xworkz.java_programs.programmingaptiude;
+
+public class Ternarythreeoperator {
+
+	public static void main(String args[]) {
+		int a=20;
+		int b=55;
+		int c=49;
+		int z=a>b?a:b;
+		int result=b>c?b:c;
+		System.out.println("value of largest num =" + result);
+}
+
+}
